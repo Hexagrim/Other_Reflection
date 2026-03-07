8ee50efd27e01e27c9323605fbe5552665342538
Hexagrim <hexagrim3456@gmail.com>
2026-03-07 17:43:28 +0545

    Hour 10, Finished game, rn im in campfire lmao, anyways did alot of bugfixing and playtesting yay

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -12,7 +12,7 @@ public class EndScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //yes this like the most uselsss code but ig its usper duperrr useful and o better get 10x mltiplier for this shit BRUH milkiway is so poggs
     }
     public void MainMenu()
     {
